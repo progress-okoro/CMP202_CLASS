8e1723021e1376e37759f9c7e3c55a43fd97d4d1
progress-okoro <progress.okoro@binghamuni.edu.ng>
2023-05-12 16:22:55 +0100

    append class code

--- src/files/FileWriterMain.java
+++ src/files/FileWriterMain.java
@@ -17,6 +17,10 @@ public class FileWriterMain
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             PrintWriter printWriter = new PrintWriter(bufferedWriter);
 
+            printWriter.println(data);
+            printWriter.println("more money more problem");
+            printWriter.println("no money no problem");
+
 
         writeToFile.flush();
         writeToFile.close();
@@ -32,7 +36,7 @@ public class FileWriterMain
         try {
             File myfile = new File("test.txt");
             Scanner myReader = new Scanner(myfile);
-            while (myReader.hasNext()){
+            while (myReader.hasNext()) {
                 String dataInFile = myReader.nextLine();
                 System.out.println(dataInFile);
             }
@@ -40,9 +44,24 @@ public class FileWriterMain
             System.out.println("can not read from this file");
         }
 
+        public void appendData(String data){
+            try {
+                File myfile = new File("test.txt");
+                FileWriter fileWriter = new FileWriter(myfile);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                PrintWriter printWriter = new PrintWriter(bufferedWriter);
 
+                printWriter.println(data);
+                printWriter.println("more money more problem");
+                printWriter.println("no money still problem");
 
+              //  writeToFile.flush();
+               // writeToFile.close();
+            }catch(IOException ioException){
+                System.out.println("can not write to this file");
+            }
 
+        }
     }
 }
 
